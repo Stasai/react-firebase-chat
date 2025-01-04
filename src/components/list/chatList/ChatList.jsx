27eef1020b8b86f4a1081a3c1f15d921583d5e1b
Stasai <85591887+Stasai@users.noreply.github.com>
2025-01-03 16:54:09 -0800

    complete addUser

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./chatlist.css"
+import AddUser from "./addUser/addUser";
 
 const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
@@ -40,6 +41,7 @@ const ChatList = () => {
                     <p>Hello</p>
                 </div>
             </div>
+            {addMode && <AddUser />}
         </div>
     )
 }
